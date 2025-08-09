6b642148ae691486ff8fc07722364a43de38164e
Mohammad Nejati <ashtumashtum@gmail.com>
2025-08-09 09:41:10 +0000

    Fix use-after-move bugs when calling async_immediate

--- include/boost/http_io/impl/read.hpp
+++ include/boost/http_io/impl/read.hpp
@@ -141,8 +141,10 @@ public:
                     BOOST_ASIO_HANDLER_LOCATION((
                         __FILE__, __LINE__,
                         "immediate"));
+                    auto io_ex = self.get_io_executor();
                     asio::async_immediate(
-                        self.get_io_executor(), asio::append(std::move(self), ec));
+                        io_ex,
+                        asio::append(std::move(self), ec));
                 }
                 goto upcall;
             }
