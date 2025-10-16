22b263cd0af51549affdb0fa10897535afce76c7
Mohammad Nejati <ashtumashtum@gmail.com>
2025-10-16 21:00:56 +0000

    refactor read operations

--- include/boost/http_io/read.hpp
+++ include/boost/http_io/read.hpp
@@ -16,12 +16,63 @@
 #include <boost/asio/async_result.hpp>
 #include <boost/system/error_code.hpp>
 #include <boost/system/result.hpp>
-#include <cstdint>
 
 namespace boost {
 namespace http_io {
 
 /** Read a complete header from the stream.
+
+    This function is used to asynchronously read a complete
+    message header from a stream into an instance of parser.
+    The function call always returns immediately. The
+    asynchronous operation will continue until one of the
+    following conditions is true:
+
+    @li The parser reads the header section of the message.
+    @li An error occurs.
+
+    This operation is implemented in terms of zero or more
+    calls to the stream's `async_read_some` function, and is
+    known as a <em>composed operation</em>. The program must
+    ensure that the stream performs no other reads until
+    this operation completes.
+
+    @param stream The stream from which the data is to be
+    read. The type must meet the <em>AsyncReadStream</em>
+    requirements.
+
+    @param parser The parser to use. The object must remain
+    valid at least until the handler is called; ownership is
+    not transferred.
+
+    @param token The completion token that will be used to
+    produce a completion handler, which will be called when
+    the read completes. The function signature of the
+    completion handler must be:
+    @code
+    void handler(
+        error_code const& error,        // result of operation
+        std::size_t bytes_transferred   // the number of bytes consumed by the parser
+    );
+    @endcode
+    Regardless of whether the asynchronous operation
+    completes immediately or not, the completion handler
+    will not be invoked from within this function. On
+    immediate completion, invocation of the handler will be
+    performed in a manner equivalent to using
+    `asio::async_immediate`.
+
+    @par Per-Operation Cancellation
+
+    This asynchronous operation supports cancellation for
+    the following `asio::cancellation_type` values:
+
+    @li `asio::cancellation_type::terminal`
+    @li `asio::cancellation_type::partial`
+    @li `asio::cancellation_type::total`
+
+    if they are also supported by the AsyncReadStream type's
+    async_read_some operation.
 */
 template<
     class AsyncReadStream,
@@ -38,9 +89,61 @@ async_read_header(
         BOOST_ASIO_DEFAULT_COMPLETION_TOKEN(
             typename AsyncReadStream::executor_type));
 
-/** Read some of the message body from the stream
+/** Read part of the message body from the stream.
 
-    @throws std::logic_error `pr.got_header() == false`
+    This function is used to asynchronously read part of a
+    message body from a stream into an instance of parser.
+    The function call always returns immediately. The
+    asynchronous operation will continue until one of the
+    following conditions is true:
+
+    @li The parser reads the header section of the message.
+    @li The parser reads the entire message.
+    @li Additional body data becomes available.
+    @li An error occurs.
+
+    This operation is implemented in terms of zero or more
+    calls to the stream's `async_read_some` function, and is
+    known as a <em>composed operation</em>. The program must
+    ensure that the stream performs no other reads until
+    this operation completes.
+
+    @param stream The stream from which the data is to be
+    read. The type must meet the <em>AsyncReadStream</em>
+    requirements.
+
+    @param parser The parser to use. The object must remain
+    valid at least until the handler is called; ownership is
+    not transferred.
+
+    @param token The completion token that will be used to
+    produce a completion handler, which will be called when
+    the read completes. The function signature of the
+    completion handler must be:
+    @code
+    void handler(
+        error_code const& error,        // result of operation
+        std::size_t bytes_transferred   // the number of bytes consumed by the parser
+    );
+    @endcode
+    Regardless of whether the asynchronous operation
+    completes immediately or not, the completion handler
+    will not be invoked from within this function. On
+    immediate completion, invocation of the handler will be
+    performed in a manner equivalent to using
+    `asio::async_immediate`.
+
+    @par Per-Operation Cancellation
+
+    This asynchronous operation supports cancellation for
+    the following `asio::cancellation_type` values:
+
+    @li `asio::cancellation_type::terminal`
+    @li `asio::cancellation_type::partial`
+    @li `asio::cancellation_type::total`
+
+    if they are also supported by the AsyncReadStream type's
+    async_read_some operation.
 */
 template<
     class AsyncReadStream,
@@ -57,9 +160,59 @@ async_read_some(
         BOOST_ASIO_DEFAULT_COMPLETION_TOKEN(
             typename AsyncReadStream::executor_type));
 
-/** Read the complete message body from the stream
+/** Read a complete message from the stream.
 
-    @throws std::logic_error `pr.got_header() == false`
+    This function is used to asynchronously read a complete
+    message from a stream into an instance of parser.
+    The function call always returns immediately. The
+    asynchronous operation will continue until one of the
+    following conditions is true:
+
+    @li The parser reads the entire message.
+    @li An error occurs.
+
+    This operation is implemented in terms of zero or more
+    calls to the stream's `async_read_some` function, and is
+    known as a <em>composed operation</em>. The program must
+    ensure that the stream performs no other reads until
+    this operation completes.
+
+    @param stream The stream from which the data is to be
+    read. The type must meet the <em>AsyncReadStream</em>
+    requirements.
+
+    @param parser The parser to use. The object must remain
+    valid at least until the handler is called; ownership is
+    not transferred.
+
+    @param token The completion token that will be used to
+    produce a completion handler, which will be called when
+    the read completes. The function signature of the
+    completion handler must be:
+    @code
+    void handler(
+        error_code const& error,        // result of operation
+        std::size_t bytes_transferred   // the number of bytes consumed by the parser
+    );
+    @endcode
+    Regardless of whether the asynchronous operation
+    completes immediately or not, the completion handler
+    will not be invoked from within this function. On
+    immediate completion, invocation of the handler will be
+    performed in a manner equivalent to using
+    `asio::async_immediate`.
+
+    @par Per-Operation Cancellation
+
+    This asynchronous operation supports cancellation for
+    the following `asio::cancellation_type` values:
+
+    @li `asio::cancellation_type::terminal`
+    @li `asio::cancellation_type::partial`
+    @li `asio::cancellation_type::total`
+
+    if they are also supported by the AsyncReadStream type's
+    async_read_some operation.
 */
 template<
     class AsyncReadStream,
